8c36c12d1812e3bddfaebc4a5ac0b73c0fb0be1b
Aditya Agarwal <aditya.agarwal030504@gmail.com>
2024-01-25 23:02:17 +0530

    feat: upload code
    Signed-off-by: Aditya Agarwal <aditya.agarwal030504@gmail.com>

---
+++ src/constants.h
@@ -0,0 +1,31 @@
+// display constants
+
+const uint16_t BLACK   = 0x0000;
+const uint16_t BLUE    = 0x001F;
+const uint16_t RED     = 0xF800;
+const uint16_t GREEN   = 0x07E0;
+const uint16_t CYAN    = 0x07FF;
+const uint16_t MAGENTA = 0xF81F;
+const uint16_t YELLOW  = 0xFFE0;
+const uint16_t WHITE   = 0xFFFF;
+
+const uint16_t DISPLAY_WIDTH   = 320;
+const uint16_t DISPLAY_HEIGHT  = 480;
+
+// touch constants
+
+const uint16_t PRESSURE_LEFT   = 10;
+const uint16_t PRESSURE_RIGHT  = 1200;
+
+const int XP = 8;
+const int XM = A2;
+const int YP = A3;
+const int YM = 9;
+
+const uint16_t CALIBRATION_TOUCH_CNT       = 5;
+const uint16_t CALIBRATION_TOUCH_PERIOD_MS = 600;
+
+// application constants
+
+const uint16_t CROSSHAIR_W = 17;
+const uint16_t CROSSHAIR_H = 17;
